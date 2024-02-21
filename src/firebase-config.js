c0a7b750b02f6ee00bbfc872374f43055d3efdfe
I210054-Carlos Santos <i210054@INETE.NET>
2024-02-21 14:03:41 +0000

    Adding firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5YxvhDgNidEnS16cdqBxbJWbvNrYkZzo",
+  authDomain: "gestin-bdf16.firebaseapp.com",
+  projectId: "gestin-bdf16",
+  storageBucket: "gestin-bdf16.appspot.com",
+  messagingSenderId: "774999163742",
+  appId: "1:774999163742:web:50627a33532c904d3645cf"
+};
+
+const app = initializeApp(firebaseConfig);
