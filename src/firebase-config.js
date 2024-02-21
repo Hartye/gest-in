cdfce6171327caa7631affce4f4ddda39a77db97
I210054-Carlos Santos <i210054@INETE.NET>
2024-02-21 14:20:51 +0000

    Adding firestore

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD5YxvhDgNidEnS16cdqBxbJWbvNrYkZzo",
@@ -10,3 +11,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
