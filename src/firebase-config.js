13ae0d872e7bf62a082a32f0c0dfe53a96440dd3
Hartye <hartye.lilith@gmail.com>
2024-03-03 13:59:53 +0000

    Adding an alternative api url for an alternative firebase project

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,13 +1,23 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD5YxvhDgNidEnS16cdqBxbJWbvNrYkZzo",
+//   authDomain: "gestin-bdf16.firebaseapp.com",
+//   projectId: "gestin-bdf16",
+//   storageBucket: "gestin-bdf16.appspot.com",
+//   messagingSenderId: "774999163742",
+//   appId: "1:774999163742:web:50627a33532c904d3645cf"
+// };
+
+// Alternative firebase project
 const firebaseConfig = {
-  apiKey: "AIzaSyD5YxvhDgNidEnS16cdqBxbJWbvNrYkZzo",
-  authDomain: "gestin-bdf16.firebaseapp.com",
-  projectId: "gestin-bdf16",
-  storageBucket: "gestin-bdf16.appspot.com",
-  messagingSenderId: "774999163742",
-  appId: "1:774999163742:web:50627a33532c904d3645cf"
+  apiKey: "AIzaSyDXXqtk4MrZGxwwEDzgTNXV29PEHAzSLG8",
+  authDomain: "gestin-alternative.firebaseapp.com",
+  projectId: "gestin-alternative",
+  storageBucket: "gestin-alternative.appspot.com",
+  messagingSenderId: "581857994149",
+  appId: "1:581857994149:web:e8df39157cc203f918b44c"
 };
 
 const app = initializeApp(firebaseConfig);
